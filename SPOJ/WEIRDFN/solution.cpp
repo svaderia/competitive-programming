45d8e12e26e0f59f278f2013611fc659e024572c
53 68 79 61 6D 61 6C <vaderiashyamal@gmail.com>
2019-05-16 17:01:36 +0530

    Heap Practice 4
    [1] https://www.spoj.com/problems/WEIRDFN/

---
+++ SPOJ/WEIRDFN/solution.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int lli;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<lli> vli;
+typedef vector<pii> vii;
+
+const int MOD	= 1e9 + 7;
+const double PI = acos(-1.0);
+
+#define fastio			ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define sz(a) int((a).size())
+#define pb push_back
+#define mp make_pair
+#define all(c) (c).begin(),(c).end()
+#define tr(c,i) for(typeof((c).begin()) i = (c).begin(); i != (c).end(); i++)
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+#define rep(i, n)    for(int i = 0; i < (n); ++i)
+#define repA(i, a, n)  for(int i = a; i <= (n); ++i)
+#define repD(i, a, n)  for(int i = a; i >= (n); --i)
+#define fill(a)  memset(a, 0, sizeof (a))
+
+double tick(){static clock_t oldt,newt=clock();double diff=1.0*(newt-oldt)/CLOCKS_PER_SEC;oldt=newt;return diff;}
+template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);}
+template <typename T> T lcm(T a, T b){return (a*b)/gcd(a,b); }
+template<typename T>T power(T e, T n, T m){T x=1,p=e;while(n){if(n&1)x=mul(x,p,m);p=mul(p,p,m);n>>=1;}return x;}
+int mod_neg(int a, int b, int c){int res;if(abs(a-b)<c)res=a-b;else res=(a-b)%c;return(res<0?res+c:res);}
+template<typename T>T extended_euclid(T a, T b, T &x, T &y){T xx=0,yy=1;y=0;x=1;while(b){T q=a/b,t=b;b=a%b;a=t;\
+t=xx;xx=x-q*xx;x=t;t=yy;yy=y-q*yy;y=t;}return a;}
+template<typename T>T mod_inverse(T a, T n){T x,y,z=0;T d=extended_euclid(a,n,x,y);return(d>1?-1:mod_neg(x,z,n));}
+
+vli f;
+
+lli insert_med(lli x, priority_queue<lli, vli, greater<lli> > &pmin, priority_queue<lli> &pmax){
+    int n = f.size();
+    int ind = ceil(n/(double)2);
+    if(ind > pmax.size()){
+        pmin.push(x);
+        pmax.push(pmin.top());
+        pmin.pop();
+    }else{
+        if(pmin.size() != 0 && pmax.top() > x){
+            pmin.push(pmax.top());
+            pmax.pop();
+            pmax.push(x);
+        }else{
+            pmin.push(x);
+        }
+    }
+
+    return pmax.top();
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+		freopen("test", "r", stdin);
+	#endif
+    fastio;
+    
+    int T;
+    cin >> T;
+
+    while(T-- > 0){
+        lli a, b, c;
+        cin >> a >> b >> c;
+        int N;
+        cin >> N;
+
+        priority_queue<lli, vli, greater<lli> > pmin;
+        priority_queue<lli> pmax;
+
+
+        f.clear();
+        f.pb(1);
+        lli curr_mid = 1;
+        pmax.push(1);
+        repA(i, 2, N){
+            lli new_ele = ((a*curr_mid)%MOD + b * i + c) % MOD;
+            f.pb(new_ele);
+            curr_mid = insert_med(new_ele, pmin, pmax);
+            // cout << curr_mid << " ";
+        }
+
+        lli sum = 0;
+        tr(f, it){
+            sum += *it;
+        }
+
+        cout << sum << endl;
+
+    }
+    // cout<<"Execution time : "<<tick()<<"\n";
+    return 0;
+}
