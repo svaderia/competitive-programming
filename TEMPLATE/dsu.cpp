d13a07990aefb9a3e431d8e21a13efd4375b8789
53 68 79 61 6D 61 6C <vaderiashyamal@gmail.com>
2019-05-30 21:59:08 +0530

    Template for DSU added

---
+++ TEMPLATE/dsu.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int lli;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<lli> vli;
+typedef vector<pii> vii;
+
+#define rep(i, n)    for(int i = 0; i < (n); ++i)
+#define repA(i, a, n)  for(int i = a; i <= (n); ++i)
+#define repD(i, a, n)  for(int i = a; i >= (n); --i)
+#define fill(a)  memset(a, 0, sizeof (a))
+#define tr(c,i) for(typeof((c).begin()) i = (c).begin(); i != (c).end(); i++)
+#define pb push_back
+
+int id[100005];
+int sz[100005];
+
+int root(int p){
+    while(p != id[p]){
+        id[p] = id[id[p]];
+        p = id[p];
+    }
+    return p;
+}
+
+void unite(int a, int b){
+    int p1 = root(a);
+    int p2 = root(b);
+    if(p1 != p2){
+        if(sz[p1] < sz[p2]){
+            id[p1] = p2;
+            sz[p2] += sz[p1];
+        }else{
+            id[p2] = p1;
+            sz[p1] += sz[p2];
+        }
+    }
+}
+
+void init_dsu(int N){
+    rep(i, N){
+        id[i] = i;
+        sz[i] = 1;
+    }
+}
