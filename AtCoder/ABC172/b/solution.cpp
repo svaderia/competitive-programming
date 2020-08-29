6dc4140264a634e4ee960c4f42366e4da7b647c2
svaderia <vaderiashyamal@gmail.com>
2020-08-30 03:13:07 +0530

    Virtual Participation ABC172

---
+++ AtCoder/ABC172/b/solution.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int lli;
+#define int lli
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vi> vvi;
+typedef vector<lli> vli;
+typedef vector<pii> vii;
+
+const int MOD   = 1e9 + 7;
+const double PI = acos(-1.0);
+const int xd[4] = {1, 0, -1, 0}, yd[4] = {0, 1, 0, -1};
+const int INF   = 1e18;
+
+#define fastio            ios_base::sync_with_stdio(false);cin.tie(0)
+#define sz(a) int((a).size())
+#define rsz resize
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define eb emplace_back
+#define f first
+#define s second
+#define all(c) (c).begin(),(c).end()
+#define rall(c) (c).rbegin(),(c).rend()
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+#define rep(i, n)    for(int i = 0; i < (n); ++i)
+#define repA(i, a, n)  for(int i = a; i <= (n); ++i)
+#define repD(i, a, n)  for(int i = a; i >= (n); --i)
+#define endl "\n"
+#define spc " "
+template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);}
+template <typename T> T lcm(T a, T b){return (a*b)/gcd(a,b); }
+
+void solve(){
+
+    string s, t;
+    cin >> s >> t;
+    int n = sz(s);
+    int ans = 0;
+    rep(i, n){
+        if(s[i] != t[i]){
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}
+
+signed main(){
+    fastio;
+
+    solve();
+
+    return 0;
+}
---
+++ AtCoder/ABC172/e/solution.cpp
@@ -0,0 +1,161 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int lli;
+#define int lli
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vi> vvi;
+typedef vector<lli> vli;
+typedef vector<pii> vii;
+
+const int MOD   = 1e9 + 7;
+const double PI = acos(-1.0);
+const int xd[4] = {1, 0, -1, 0}, yd[4] = {0, 1, 0, -1};
+const int INF   = 1e18;
+
+#define fastio            ios_base::sync_with_stdio(false);cin.tie(0)
+#define sz(a) int((a).size())
+#define rsz resize
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define eb emplace_back
+#define f first
+#define s second
+#define all(c) (c).begin(),(c).end()
+#define rall(c) (c).rbegin(),(c).rend()
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+#define rep(i, n)    for(int i = 0; i < (n); ++i)
+#define repA(i, a, n)  for(int i = a; i <= (n); ++i)
+#define repD(i, a, n)  for(int i = a; i >= (n); --i)
+#define endl "\n"
+#define spc " "
+template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);}
+template <typename T> T lcm(T a, T b){return (a*b)/gcd(a,b); }
+
+#define ll lli
+
+ll euclid(ll a, ll b, ll &x, ll &y) {
+    if (b) { ll d = euclid(b, a % b, y, x);
+        return y -= a/b * x, d; }
+    return x = 1, y = 0, a;
+}
+
+const ll mod = 1000000007;
+/* template<long long mod = 1000000007> */
+struct modint{
+    long long a;
+    bool used;
+
+    modint() : a(0), used(false){}
+    modint(long long t){
+        a = t % mod;
+        if(a < 0) a += mod;
+    }
+
+    operator long long() const{ return a; }
+
+    bool operator==(const modint &x) const{ return a == x.a; }
+    bool operator!=(const modint &x) const{ return a != x.a; }
+
+    modint operator-() const{ return modint(a ? (mod - a) : 0); }
+    modint operator~() const{ return invert(*this); }
+
+    modint operator+(const modint &x) const{ return modint(*this) += x; }
+    modint operator-(const modint &x) const{ return modint(*this) -= x; }
+    modint operator*(const modint &x) const{ return modint(*this) *= x; }
+    modint operator/(const modint &x) const{ return modint(*this) /= x; }
+
+    modint &operator+=(const modint &x){
+        a += x.a;
+        if(a >= mod) a -= mod;
+        return *this;
+    }
+    modint &operator-=(const modint &x){
+        a -= x.a;
+        if(a < 0) a += mod;
+        return *this;
+    }
+    modint &operator*=(const modint &x){
+        a = a * x.a % mod;
+        return *this;
+    }
+    modint &operator/=(const modint &x){
+        a = a * (~x).a % mod;
+        return *this;
+    }
+
+    friend ostream &operator<<(ostream &os,const modint &x){
+        return os << x.a;
+    }
+    friend istream &operator>>(istream &is,modint &x){
+        long long t;
+        is >> t;
+        x = modint(t);
+        return is;
+    }
+
+    modint pow(long long x) const{
+        modint ret = 1,tmp = a;
+        for(;x;tmp *= tmp,x >>= 1){
+            if(x & 1) ret *= tmp;
+        }
+        return ret;
+    }
+
+    modint invert(modint z) const{
+        ll x, y, g = euclid(z.a, mod, x, y);
+        assert(g == 1); return modint((x + mod) % mod);
+    }
+};
+
+template<class T>
+struct Combination{
+    vector<T> fact,factinv;
+    Combination(int n) : fact(n + 1),factinv(n + 1){
+        fact[0] = 1;
+        for(int i = 1;i <= n;i++) fact[i] = fact[i - 1] * T(i);
+        factinv[n] = ~fact[n];
+        for(int i = n;i >= 1;i--) factinv[i - 1] = factinv[i] * T(i);
+    }
+    T nCr(int n,int r){
+        if(r < 0 || n < r) return 0;
+        return fact[n] * factinv[r] * factinv[n - r];
+    }
+};
+
+void solve(){
+
+    int n, m;
+    cin >> n >> m;
+
+    modint ans(0);
+    Combination<modint> ncr = Combination<modint>(m);
+
+    modint total = ncr.nCr(m, n) * ncr.fact[n];
+
+    modint temp(0);
+    int sign = 1;
+
+    repA(j, 1, n){
+        temp += modint(sign) * ncr.nCr(n, j) * ncr.nCr(m - j, n - j) * ncr.fact[n - j];
+        if(sign == 1) sign = -1;
+        else sign = 1;
+    }
+
+    ans = ncr.nCr(m, n) * ncr.fact[n] * (total - temp);
+
+    cout << ans << endl;
+
+}
+
+signed main(){
+    fastio;
+
+    solve();
+
+    return 0;
+}
