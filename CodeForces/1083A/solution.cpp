15f7265fc4f3fac848225bc475bcd64b752ac974
svaderia <vaderiashyamal@gmail.com>
2020-09-09 14:16:45 +0530

    Practicing on ladder
    [1] https://competitive.herokuapp.com/ladders/

---
+++ CodeForces/1083A/solution.cpp
@@ -0,0 +1,157 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int lli;
+#define int lli
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vi> vvi;
+typedef vector<lli> vli;
+typedef vector<pii> vii;
+
+const int MOD   = 1e9 + 7;
+const double PI = acos(-1.0);
+const int xd[4] = {1, 0, -1, 0}, yd[4] = {0, 1, 0, -1};
+const int INF   = 1e18;
+
+#define fastio            ios_base::sync_with_stdio(false);cin.tie(0)
+#define sz(a) int((a).size())
+#define rsz resize
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define eb emplace_back
+#define f first
+#define s second
+#define all(c) (c).begin(),(c).end()
+#define rall(c) (c).rbegin(),(c).rend()
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+#define rep(i, n)    for(int i = 0; i < (n); ++i)
+#define repA(i, a, n)  for(int i = a; i <= (n); ++i)
+#define repD(i, a, n)  for(int i = a; i >= (n); --i)
+#define endl "\n"
+#define spc " "
+template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);}
+template <typename T> T lcm(T a, T b){return (a*b)/gcd(a,b); }
+
+
+// TO_STRING
+string to_string(char c) { return string(1,c); }
+string to_string(const char* s) { return (string)s; }
+string to_string(string s) { return s; }
+string to_string(bool b) {
+    #ifdef DEBUG
+        return b ? "true" : "false";
+    #else
+        return to_string((int)b);
+    #endif
+}
+template<class A> string to_string(complex<A> c) {
+    stringstream ss; ss << c; return ss.str(); }
+string to_string(vector<bool> v) {
+    string res = "{"; rep(i,sz(v)) res += char('0'+v[i]);
+    res += "}"; return res; }
+template<size_t SZ> string to_string(bitset<SZ> b) {
+    string res = ""; rep(i,SZ) res += char('0'+b[i]);
+    return res; }
+template<class A, class B> string to_string(pair<A,B> p);
+template<class T> string to_string(T v) { // containers with begin(), end()
+    #ifdef DEBUG
+        bool fst = 1; string res = "{";
+        for (const auto& x: v) {
+            if (!fst) res += ", ";
+            fst = 0; res += to_string(x);
+        }
+        res += "}"; return res;
+    #else
+        bool fst = 1; string res = "";
+        for (const auto& x: v) {
+            if (!fst) res += " ";
+            fst = 0; res += to_string(x);
+        }
+        return res;
+
+    #endif
+}
+template<class A, class B> string to_string(pair<A,B> p) {
+    #ifdef DEBUG
+        return "("+to_string(p.f)+", "+to_string(p.s)+")";
+    #else
+        return to_string(p.f)+" "+to_string(p.s);
+    #endif
+}
+
+// DEBUG
+void DBG() { cerr << "]" << endl; }
+template<class H, class... T> void DBG(H h, T... t) {
+    cerr << to_string(h); if (sizeof...(t)) cerr << ", ";
+    DBG(t...); }
+#ifdef DEBUG // compile with -DDEBUG
+    #define dbg(...) cerr << "LINE(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+#else
+    #define dbg(...) 0
+#endif
+
+int n;
+vi w;
+vector<vii> g;
+vi dp;
+int ans;
+
+void dfs(int root, int p){
+    int m1 = 0, m2 = 0;
+
+    for(auto child : g[root]){
+        if(child.f != p){
+            dfs(child.f, root);
+            int x = dp[child.f] - child.s;
+            vi v = {m1, m2, x};
+            sort(rall(v));
+            m1 = v[0], m2 = v[1];
+            dbg(child, x, m1, m2);
+        }
+    }
+
+    ans = max(ans, m1 + m2 + w[root]);
+    dp[root] = m1 + w[root];
+}
+
+
+
+void solve(){
+
+    cin >> n;
+
+    w.rsz(n);
+    g.rsz(n);
+    dp.rsz(n);
+
+    rep(i, n){
+        cin >> w[i];
+    }
+
+    rep(i, n - 1){
+        int u, v, c;
+        cin >> u >> v >> c;
+        u--; v--;
+        g[u].pb({v, c});
+        g[v].pb({u, c});
+    }
+
+    ans = 0;
+    dfs(0, 0);
+
+    cout << ans << endl;
+
+
+}
+
+signed main(){
+    fastio;
+
+    solve();
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Competitive Coding
 |Online Judge|Solved|
 |------ | ------|
-|CodeChef | 112 |  
-|CodeForces | 149 |  
+|CodeChef | 113 |  
+|CodeForces | 154 |  
 |SPOJ | 56 |  
 |AtCoder | 88 |  
 |Leetcode | 30 |  
 |TopCoder | 5 |  
 |UVa | 3 |  
 |Other | 4 |  
-|Total | 447 |
+|Total | 453 |
