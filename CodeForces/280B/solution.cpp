21f9a3f31e86d599d0eefd3d847ab66ef4305bd0
svaderia <vaderiashyamal@gmail.com>
2020-10-05 02:30:17 +0530

    Practice

--- CodeForces/280B/solution.cpp
+++ CodeForces/280B/solution.cpp
@@ -14,7 +14,6 @@ const double PI = acos(-1.0);
 #define present(c,x) ((c).find(x) != (c).end())
 #define cpresent(c,x) (find(all(c),x) != (c).end())
 
-double tick(){static clock_t oldt,newt=clock();double diff=1.0*(newt-oldt)/CLOCKS_PER_SEC;oldt=newt;return diff;}
 template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);}
 template <typename T> T lcm(T a, T b){return (a*b)/gcd(a,b); }
 int mod_neg(int a, int b, int c){int res;if(abs(a-b)<c)res=a-b;else res=(a-b)%c;return(res<0?res+c:res);}
@@ -22,14 +21,9 @@ template<typename T>T extended_euclid(T a, T b, T &x, T &y){T xx=0,yy=1;y=0;x=1;
 t=xx;xx=x-q*xx;x=t;t=yy;yy=y-q*yy;y=t;}return a;}
 template<typename T>T mod_inverse(T a, T n){T x,y,z=0;T d=extended_euclid(a,n,x,y);return(d>1?-1:mod_neg(x,z,n));}
 
-
-
 int main(){
-    #ifndef ONLINE_JUDGE
-		freopen("test", "r", stdin);
-	#endif
     fastio;
-    
+
     int N;
     cin >> N;
 
@@ -44,10 +38,10 @@ int main(){
     lli max = -1;
     for(int i = 0; i < N; i++){
         while(!s.empty() && arr[i] > s.top()){
-            // lli now = s.top();
+            lli now = s.top();
             s.pop();
-            // lli temp1 = now ^ arr[i];
-            // if(temp1 > max) max = temp1;
+            lli temp1 = now ^ arr[i];
+            if(temp1 > max) max = temp1;
         }
         if(!s.empty()){
             lli now = s.top();
