55690ceccf6d550d88c9a5e88b21b71b9ff281d8
svaderia <vaderiashyamal@gmail.com>
2020-10-08 17:04:42 +0530

    Template for DSU Updated

--- TEMPLATE/dsu.h
+++ TEMPLATE/dsu.h
@@ -1,44 +1,31 @@
-//Class to implement union find using path compression
-//1-based indexing assumed
+// 0-based indexing assumed
 
-class UnionFind {
-private:
-    int n, set_size, *parent, *rank;
-public:
-    //Complexity: O(n)
-    UnionFind(int a) {
-        n = set_size = a;
-        parent = new int[n+1];
-        rank = new int[n+1];
-        for(int i = 1; i <= n; ++i) parent[i]=i, rank[i]=1;
+struct UnionFind {
+    int n, set_size;
+    vi parent, rank;
+
+    UnionFind(int a) : n(a), set_size(n), parent(n), rank(n, 1) {
+        rep(i, n) parent[i]=i;
     }
-    ~UnionFind() {
-        delete rank;
-        delete parent;
-    }
-    //Path Compression
+
     int find(int x) {
         if (x != parent[x]) return parent[x] = find(parent[x]);
         return x;
     }
-    //Complexity : O(A N), where A = Inverse Ackermann function
-    void unite(int x, int y) {
+
+    bool merge(int x, int y) {
         int xroot = find(x), yroot = find(y);
-        if (xroot != yroot) {
-            if (rank[xroot] >= rank[yroot]) {
-                parent[yroot] = xroot;
-                rank[xroot] += rank[yroot];
-            }
-            else {
-                parent[xroot] = yroot;
-                rank[yroot] += rank[xroot];
-            }
-            set_size -= 1;
-        }
+        if(xroot == yroot) return false;
+        if (rank[xroot] < rank[yroot]) swap(xroot, yroot);
+        parent[yroot] = xroot;
+        rank[xroot] += rank[yroot];
+        set_size -= 1;
+        return true;
     }
+
+    bool same(int x, int y){
+        return find(x) == find(y);
+    }
+
     int size() { return set_size; }
-    void print() {
-        for(int i = 1; i <= n; ++i)
-            cout << i << " -> " << parent[i] << "\n";
-    }
 };
