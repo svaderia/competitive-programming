c0a9265d73e542fb115701361d29e95569662e5c
svaderia <vaderiashyamal@gmail.com>
2024-03-03 19:34:10 -0500

    Codeforces Round 931 (Div. 2)

---
+++ CodeForces/1938/b/solution.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int lli;
+#define int lli
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vi> vvi;
+typedef vector<lli> vli;
+typedef vector<pii> vii;
+
+const int MOD   = 1e9 + 7;
+const double PI = acos(-1.0);
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const lli INF   = 1e18;
+
+#define fastio            ios_base::sync_with_stdio(false);cin.tie(0)
+#define sz(a) int((a).size())
+#define rsz resize
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define eb emplace_back
+#define f first
+#define s second
+#define all(c) (c).begin(),(c).end()
+#define rall(c) (c).rbegin(),(c).rend()
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+#define rep(i, n)    for(int i = 0; i < (n); ++i)
+#define repA(i, a, n)  for(int i = a; i <= (n); ++i)
+#define repD(i, a, n)  for(int i = a; i >= (n); --i)
+// Remove this when it's an interactive problem
+#define endl "\n"
+#define spc " "
+template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);}
+template <typename T> T lcm(T a, T b){return (a*b)/gcd(a,b); }
+
+
+void solve(){
+    int n;
+    cin >> n;
+
+    vi a = {1, 3, 6, 10, 15};
+
+    int ans = INF;
+
+    repA(k1, 0, 2){
+        repA(k2, 0, 1){
+            repA(k3, 0, 2){
+                repA(k4, 0, 2){
+                    int cum  = k1 + k2 + k3 + k4;
+                    int curr = k1 * 1 + k2 * 3 + k3 * 6 + k4 * 10;
+
+                    int y = (n - curr) % 15;
+                    if(curr <= n && y == 0){
+                        ans = min(ans, (n - curr) / 15 + cum);
+                    }
+                }
+            }
+        }
+    }
+
+    cout << ans << endl;
+
+}
+
+signed main(){
+    fastio;
+
+    int T;
+    cin >> T;
+
+    repA(tc, 1, T){
+        solve();
+    }
+
+    return 0;
+}
