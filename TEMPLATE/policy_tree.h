35ca043a707e4853867127a8c80bb5df37f849ec
svaderia <vaderiashyamal@gmail.com>
2024-04-22 14:45:23 -0400

    Update in template

--- TEMPLATE/policy_tree.h
+++ TEMPLATE/policy_tree.h
@@ -2,3 +2,4 @@
 using namespace __gnu_pbds;
 
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> OrderedSet; // order_of_key find_by_order
+// Please remove #define int lli
