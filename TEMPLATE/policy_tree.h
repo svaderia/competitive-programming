e48f67fc447785bff74305a24381da18f53b38a3
svaderia <vaderiashyamal@gmail.com>
2024-05-24 12:45:35 -0400

    Updated Policy Tree template

--- TEMPLATE/policy_tree.h
+++ TEMPLATE/policy_tree.h
@@ -1,5 +1,7 @@
 #include <ext/pb_ds/assoc_container.hpp> 
 using namespace __gnu_pbds;
 
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> OrderedSet; // order_of_key find_by_order
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set; // order_of_key find_by_order
 // Please remove #define int lli
+
+typedef tree<int, int, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_map; // order_of_key find_by_order
