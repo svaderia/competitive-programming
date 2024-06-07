3470c37682d827843147a02b381ecee7dbad6c2f
svaderia <vaderiashyamal@gmail.com>
2024-06-07 13:22:15 -0400

    (cses) CSES DP Section

---
+++ Other/CSES/1158/new_solution_1717638348.cpp
@@ -0,0 +1,145 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int lli;
+#define int lli
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vi> vvi;
+typedef vector<lli> vli;
+typedef vector<pii> vii;
+
+const int MOD   = 1e9 + 7;
+const double PI = acos(-1.0);
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const lli INF   = 1e18;
+
+#define fastio            ios_base::sync_with_stdio(false);cin.tie(0)
+#define sz(a) int((a).size())
+#define rsz resize
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define eb emplace_back
+#define f first
+#define s second
+#define all(c) (c).begin(),(c).end()
+#define rall(c) (c).rbegin(),(c).rend()
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+#define rep(i, n)    for(int i = 0; i < (n); ++i)
+#define repA(i, a, n)  for(int i = a; i <= (n); ++i)
+#define repD(i, a, n)  for(int i = a; i >= (n); --i)
+// Remove this when it's an interactive problem
+#define endl "\n"
+#define spc " "
+template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);}
+template <typename T> T lcm(T a, T b){return (a*b)/gcd(a,b); }
+// clang-format on
+
+// TO_STRING
+string to_string(char c) { return string(1,c); }
+string to_string(const char* s) { return (string)s; }
+string to_string(string s) { return s; }
+string to_string(bool b) {
+    #ifdef DEBUG
+        return b ? "true" : "false";
+    #else
+        return to_string((int)b);
+    #endif
+}
+template<class A> string to_string(complex<A> c) {
+    stringstream ss; ss << c; return ss.str(); }
+string to_string(vector<bool> v) {
+    string res = "{"; rep(i,sz(v)) res += char('0'+v[i]);
+    res += "}"; return res; }
+template<size_t SZ> string to_string(bitset<SZ> b) {
+    string res = ""; rep(i,SZ) res += char('0'+b[i]);
+    return res; }
+template<class A, class B> string to_string(pair<A,B> p);
+template<class T> string to_string(T v) { // containers with begin(), end()
+    #ifdef DEBUG
+        bool fst = 1; string res = "{";
+        for (const auto& x: v) {
+            if (!fst) res += ", ";
+            fst = 0; res += to_string(x);
+        }
+        res += "}"; return res;
+    #else
+        bool fst = 1; string res = "";
+        for (const auto& x: v) {
+            if (!fst) res += " ";
+            fst = 0; res += to_string(x);
+        }
+        return res;
+
+    #endif
+}
+template<class A, class B> string to_string(pair<A,B> p) {
+    #ifdef DEBUG
+        return "("+to_string(p.f)+", "+to_string(p.s)+")";
+    #else
+        return to_string(p.f)+" "+to_string(p.s);
+    #endif
+}
+
+// DEBUG
+void DBG() { cerr << "]" << endl; }
+template<class H, class... T> void DBG(H h, T... t) {
+    cerr << to_string(h); if (sizeof...(t)) cerr << ", ";
+    DBG(t...); }
+#ifdef DEBUG // compile with -DDEBUG
+    #define dbg(...) cerr << "LINE(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+#else
+    #define dbg(...) 0
+#endif
+
+void solve() {
+    int n, x;
+    cin >> n >> x;
+
+    vi cost(n);
+    rep(i, n) {
+        cin >> cost[i];
+    }
+
+    vi page(n);
+    rep(i, n) {
+        cin >> page[i];
+    }
+
+    vi dp(x + 1, -1);
+
+    dp[0] = 0;
+
+    rep(i, n) {
+        vi tdp(x + 1, -1);
+        rep(j, x + 1) {
+            if (dp[j] != -1 && j + cost[i] <= x) {
+                tdp[j + cost[i]] = max(dp[j + cost[i]], dp[j] + page[i]);
+            }
+        }
+        rep(j, x + 1){
+            dp[j] = max(dp[j], tdp[j]);
+        }
+
+        dbg(dp);
+    }
+
+    int ans = -1;
+    rep(i, x + 1) {
+        ans = max(ans, dp[i]);
+    }
+
+    cout << ans << endl;
+}
+
+signed main() {
+    fastio;
+
+    solve();
+
+    return 0;
+}
